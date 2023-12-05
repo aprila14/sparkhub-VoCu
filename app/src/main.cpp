6ed30ab5bc8bec2452bebacb49f86559cec72500
lsawicki-wizzdev <lukasz.sawicki@wizzdev.pl>
2023-12-05 13:57:38 +0100

    KEV1-5 MAC logging changed

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -111,11 +111,13 @@ void initCommonGlobalModules()
     // ADC for pressure sensor
     adcInit();
 
-    uint8_t mac[6] = {};
+    {
+        uint8_t mac[6] = {};
 
-    wifiController.getWifiMacAddress(mac);
+        wifiController.getWifiMacAddress(mac);
 
-    LOG_ERROR("MAC - %02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+        LOG_INFO("MAC - %02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    }
 
     // run modules which are tasks
     if (!isCertificateSet)
